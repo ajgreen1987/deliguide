e6d38a7a741cfdccb2160b4dc29910c3abcac954
AJ Green <ajgreen1987@gmail.com>
2015-02-28 13:26:01 -0500

    - Adding Side Menu

--- DeliGuide/ViewController.h
+++ DeliGuide/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UITableViewController
 
+- (IBAction)unwindToMenuViewController:(UIStoryboardSegue *)segue;
 
 @end
 
